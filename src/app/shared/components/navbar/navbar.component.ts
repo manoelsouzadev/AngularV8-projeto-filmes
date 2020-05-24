674ff40ccf54bef0009065522171b40663fd3ffb
manoelsouzadev <manoel.souza280@gmail.com>
2020-05-24 01:41:23 -0300

    Adiconado destaque para página ativa no menu principal

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { Router } from "@angular/router";
 @Component({
     selector: "navbar",
     templateUrl: "./navbar.component.html",
-    styleUrls: ["./navbar.component.scss"]
+    styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
     public form: FormGroup;
@@ -13,18 +13,33 @@ export class NavbarComponent implements OnInit {
     constructor(private formBuilder: FormBuilder, private router: Router) {}
 
     ngOnInit() {
+        let linkHome = (document.getElementById("linkHomeMenu").style.color =
+            "#fff");
+
         this.form = this.formBuilder.group({
-            nome: [null, [Validators.required, Validators.minLength(2)]]
+            nome: [null, [Validators.required, Validators.minLength(2)]],
         });
     }
 
     getFilmes(): any {
         if (this.form.valid) {
             this.router.navigate(["lista-filmes"], {
-                queryParams: { search: this.form.get("nome").value }
+                queryParams: { search: this.form.get("nome").value },
             });
         } else {
             return;
         }
+    }
+
+    mostrarAtivo(tag): void {
+        let tag_a = document.getElementsByClassName("nav-link");
+        let newColor = "#646363";
+        document.getElementById("logo").style.color = "#fff";
+        for (let i = 0; i < tag_a.length; i++) {
+            document.getElementById("linkHomeMenu").style.color = newColor;
+            document.getElementById("linkSobre").style.color = newColor;
+            document.getElementById("linkApi").style.color = newColor;
+        }
+        tag.style.color = "#fff";
     }
 }
