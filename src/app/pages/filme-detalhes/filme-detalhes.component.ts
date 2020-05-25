02f6933c7b65c6f606e6f4edf5a9f5c1e65cb5cb
manoelsouzadev <manoel.souza280@gmail.com>
2020-05-24 21:10:51 -0300

    Alteração na page de detalhes de filmes para mostrar mais detalhes dos filmes

--- src/app/pages/filme-detalhes/filme-detalhes.component.ts
+++ src/app/pages/filme-detalhes/filme-detalhes.component.ts
@@ -11,6 +11,8 @@ import { FilmeDetalhesService } from "./filme-detalhes.service";
 export class FilmeDetalhesComponent implements OnInit {
     private id: string;
     public detalhesFilme: any[];
+    public actors: any[];
+    public writers: any[];
 
     constructor(
         private route: ActivatedRoute,
@@ -29,7 +31,20 @@ export class FilmeDetalhesComponent implements OnInit {
 
     getFilmes(): void {
         this.filmeDetalhesService.buscarFilme(this.id).then((res) => {
+            this.getStringValues([res]);
             this.detalhesFilme = Array.of(res);
         });
+       
+    }
+
+    getStringValues(detalhes){
+        detalhes.filter(elem => {
+            if(elem.Actors !== 'N/A')
+                this.actors = elem.Actors.split(',');
+                console.log(this.actors);
+            if(elem.Writer !== 'N/A' )
+                this.writers = elem.Writer.split(',');
+                console.log(this.writers);
+        })
     }
 }
